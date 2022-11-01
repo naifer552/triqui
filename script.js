e98b5441c55a66b01fd1af3d4903d4b30b219c1d
Naifer <naiferdavid@hotmail.com>
2022-11-01 15:14:28 -0500

    juego con maquina random terminado-falta boton reset

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let valueId, indexEliminar;
-let turn = 'x';
 let arrHum = [], arrMac = [], validar = [];
 let player, coincidentes = 0;
 arrOriginal = [1,2,3,4,5,6,7,8,9];
@@ -23,22 +22,16 @@ function listening() {
             valueId = parseInt(valueId)
             if (arrOriginal.includes(valueId)) {
                 humanPlayer();
-
-                
             }else if(arrOriginal.length>0){
                 alert('Ingrese por favor un valor valido');
             }else{
                 alert('El juego a terminado');
             }
-
-
         });
-
     }
 }
 
 function humanPlayer() {
-
     document.getElementById(`${valueId}`).innerHTML = "X";
     arrHum.push(valueId);
     arrHum.sort();
@@ -50,10 +43,13 @@ function humanPlayer() {
         validar = arrHum;
         player = 'el humano';
         validate();
-
-
     }
-    machinePlayer();
+    if (arrOriginal.length>0) {
+        setTimeout(() => {
+            machinePlayer();
+        }, 250);
+    }
+
 }
 function machinePlayer() {
     valueId = arrOriginal[Math.floor(Math.random() * arrOriginal.length)];
@@ -81,16 +77,26 @@ function validate() {
             if (validar[k] == possibleCombinations[j][0] || validar[k] == possibleCombinations[j][1] || validar[k] == possibleCombinations[j][2]) {
                 coincidentes++;
                 if (coincidentes>2) {
-                    console.log(`Gano ${player}`);
-                    arrOriginal = [];  
+                    setTimeout(() => {
+                        alert(`Gano ${player}`);
+
+                    }, 350);
+                    arrOriginal = [];
+                    reset();
+                }
+                if (coincidentes<2 && arrOriginal ==[]) {
+                    reset();
+                    
                 }
             }
         }
-    }
-    
+    } 
 }
 
-
+function reset() {
+    console.log('juego terminado');
+    
+}
 
 listening();
 
